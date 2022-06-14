df852b2d872dfe9c53bb1a66c96846e635463a0b
Madhur Goyal <madhur.goyal2@gmail.com>
2022-06-15 00:43:48 +0530

    ADD: Wikimedia Producer

---
+++ producer.js
@@ -0,0 +1,37 @@
+const kafka = require('./kafka');
+const config = require('./config');
+const EventSource = require('eventsource');
+
+const producer = kafka.producer();
+
+const main = async () => {
+  await producer.connect();
+
+  const events = new EventSource(
+    'https://stream.wikimedia.org/v2/stream/recentchange'
+  );
+
+  try {
+    events.onmessage = async (event) => {
+      const message = await producer.send({
+        topic: config.kafka.topic,
+        messages: [{ value: event.data }],
+      });
+      console.log('\n\n========== Message Produced ===========');
+      console.log(message);
+      console.log('========== ======== ===========');
+    };
+  } catch {
+    events.close();
+    await producer.disconnect();
+  }
+
+  events.onerror = async (event) => {
+    console.log('\n\n========== ERROR ===========');
+    console.log(event);
+    console.log('========== ======== ===========');
+    await producer.disconnect();
+  };
+};
+
+main();
